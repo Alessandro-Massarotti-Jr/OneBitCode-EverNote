5c1147c70e41602ebd9837494478aa9320ea6beb
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-07-07 22:06:17 -0300

    User login route

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken";
+import jwt, { Secret } from "jsonwebtoken";
 
 import { Request, Response } from "express"
 import { ReturnAPI } from "../resources/returnAPI";
@@ -12,15 +12,15 @@ class UserController {
     public async create(req: Request, res: Response) {
 
         if (!req.body.name) {
-          const returnAPI = new ReturnAPI(false,'nome é obrigatorio',{});
-          return res.status(400).json(returnAPI);
+            const returnAPI = new ReturnAPI(false, 'nome é obrigatorio', {});
+            return res.status(400).json(returnAPI);
         }
         if (!req.body.email) {
-            const returnAPI = new ReturnAPI(false,'email é obrigatorio',{});
+            const returnAPI = new ReturnAPI(false, 'email é obrigatorio', {});
             return res.status(400).json(returnAPI);
         }
         if (!req.body.password) {
-            const returnAPI = new ReturnAPI(false,'senha é obrigatorio',{});
+            const returnAPI = new ReturnAPI(false, 'senha é obrigatorio', {});
             return res.status(400).json(returnAPI);
         }
         const userPassword = req.body.password;
@@ -33,16 +33,84 @@ class UserController {
                     email: req.body.email,
                     password: passwordhash,
                     name: req.body.name
+                },
+                select: {
+                    email: true,
+                    name: true,
+                    id: true
                 }
             })
 
             async () => { await prisma.$disconnect(); }
 
-            const returnAPI = new ReturnAPI(true,"usuario cadastrado com sucesso",newUser)
+            const returnAPI = new ReturnAPI(true, "usuario cadastrado com sucesso", newUser)
             return res.status(201).json(returnAPI);
 
         } catch (err: any) {
-            const returnAPI = new ReturnAPI(false,err.message,{})
+            const returnAPI = new ReturnAPI(false, err.message, {})
+            return res.status(500).json(returnAPI);
+        }
+    }
+
+    public async login(req: Request, res: Response) {
+
+        if (!req.body.email) {
+            const returnAPI = new ReturnAPI(false, 'email é obrigatorio', {});
+            return res.status(400).json(returnAPI);
+        }
+        if (!req.body.password) {
+            const returnAPI = new ReturnAPI(false, 'senha é obrigatorio', {});
+            return res.status(400).json(returnAPI);
+        }
+
+
+        const userMail = req.body.email
+        const userPassword = req.body.password
+        try {
+            const userResult = await prisma.user.findFirst({
+                where: {
+                    email: {
+                        equals: userMail
+                    }
+                },
+                select: {
+                    password: true,
+                    email: true,
+                    name: true,
+                    id: true
+                }
+
+            });
+
+            const checkPassword = await bcrypt.compare(userPassword, userResult!.password);
+
+            if (!checkPassword) {
+                const returnAPI = new ReturnAPI(false, 'Senha Invalida', {})
+                return res.status(400).json(returnAPI);
+            }
+
+            try {
+
+                const jwt_secret = process.env.JWT_SECRET;
+
+
+                const jwt_token = jwt.sign({
+                    id: userResult!.id,
+                }, jwt_secret as Secret)
+
+                async () => {
+                    await prisma.$disconnect();
+                }
+
+                const returnAPI = new ReturnAPI(true, "Login efetuado com sucesso", { id: userResult!.id, name: userResult!.name, email: userResult!.email, token: jwt_token })
+                return res.status(200).json(returnAPI);
+
+            } catch (err: any) {
+                const returnAPI = new ReturnAPI(false, err.message, {})
+                return res.status(500).json(returnAPI);
+            }
+        } catch (err: any) {
+            const returnAPI = new ReturnAPI(false, err.message, {})
             return res.status(500).json(returnAPI);
         }
     }
--- server/src/routers/userRouter.ts
+++ server/src/routers/userRouter.ts
@@ -9,4 +9,6 @@ userRouter.get("/", async (req, res) => {
     return res.status(201).json({user:"usuario"});
 })
 
-userRouter.post("/register", userController.create )
+userRouter.post("/register", userController.create );
+
+userRouter.post("/login", userController.login );
