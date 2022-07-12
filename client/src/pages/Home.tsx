e0a1855bf1f3ec1e83572e1f47af7f01226abc65
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-07-12 19:55:25 -0300

    home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Presentation from "../assets/img/presentation.png"
 export default function Home(){
     return(
         <section className="h-screen flex justify-around items-center py-2 px-3 bg-gradient-to-tr from-custom-400 to-custom-600 text-white">
-            <div className="max-w-[50%] flex flex-col items-center justify-center">
+            <div className="max-w-[50%] flex flex-col items-start justify-center">
                 <h1 className="font-bold text-5xl">Create notes easily and access when you wants on the cloud</h1>
                 <p className="mt-4 text-xl"> Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.</p>
                 <p className="mt-4 text-xl"> Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.</p>
