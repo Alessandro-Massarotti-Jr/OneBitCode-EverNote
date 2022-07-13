9ce9e52f303709c55d9c0a82cbd2bf4209b7ff73
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-07-13 20:57:31 -0300

    responsive home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,11 +2,11 @@ import Presentation from "../assets/img/presentation.png"
 
 export default function Home(){
     return(
-        <section className="h-screen flex justify-around items-center py-2 px-3 bg-gradient-to-tr from-custom-400 to-custom-600 text-white">
-            <div className="max-w-[50%] flex flex-col items-start justify-center">
-                <h1 className="font-bold text-5xl">Create notes easily and access when you wants on the cloud</h1>
-                <p className="mt-4 text-xl"> Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.</p>
-                <p className="mt-4 text-xl"> Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.</p>
+        <section className="min-h-screen flex md:flex-row flex-col justify-around items-center md:py-2 py-12 px-3 bg-gradient-to-tr from-custom-400 to-custom-600 text-white">
+            <div className="md:max-w-[50%] flex flex-col items-start justify-center">
+                <h1 className="font-bold text-3xl md:text-5xl">Create notes easily and access when you wants on the cloud</h1>
+                <p className="mt-4 text-sm md:text-xl"> Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.</p>
+                <p className="mt-4 text-sm md:text-xl"> Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.</p>
                 <a href="" className="mt-4 text-center text-xl border-2 border-white py-2 px-4 hover:bg-white hover:text-custom-400 transition-colors rounded">Register for free now</a>
             </div>
             <div>
