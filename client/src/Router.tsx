a61122f1f5cbd1e5099066ff0741d8109ee25787
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-07-14 20:47:24 -0300

    add routes

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 
-
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 import Home from "./pages/Home"
+import UserEdit from './pages/UserEdit';
 
 
 export default function Router() {
@@ -9,6 +11,9 @@ export default function Router() {
        
             <Routes>
                 <Route path="/" element={ <Home />}></Route>
+                <Route path="/register" element={ <Register />}></Route>
+                <Route path="/login" element={ <Login />}></Route>
+                <Route path="/user/edit" element={ <UserEdit />}></Route>
             </Routes>
    
     );
