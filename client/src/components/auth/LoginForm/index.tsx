4002810ec244a042230f364313f4a4ffd34a648c
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-08-08 20:48:38 -0300

    Login Front

---
+++ client/src/components/auth/LoginForm/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import UserService from "../../../services/UserService";
+import InputText from "../../Form/InputText";
+import {useNavigate} from "react-router-dom"
+
+export default function LoginForm(){
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+   async function handleFormSubmit(event:React.SyntheticEvent){
+      event.preventDefault();
+      try{
+       await UserService.login({email:email,password:password});
+        navigate('/notes',{replace:true})
+      }catch(error){
+       console.log(error);
+       
+      } 
+    }
+
+    return(
+        <form onSubmit={(event)=>{handleFormSubmit(event)}} className="flex flex-col items-center justify-center text-zinc-600" action="">
+            <InputText value={email} changeValue={setEmail} name="Email" type="email" required={true}/>
+            <InputText value={password} changeValue={setPassword} name="Password" type="password" required={true}/>
+           <div className="flex">
+           <Link to="/register" className="text-custom-400 py-2 px-4 mx-2 my-2 md:mt-0 transition-colors hover:text-custom-600 font-bold"  replace={true}>Register</Link>
+            <button className="text-custom-400 font-bold py-2 px-4 mx-2 my-2 md:mt-0 transition-colors border border-custom-400 rounded-md hover:bg-custom-400 hover:text-white cursor-pointer"  type="submit">Login</button>
+           </div>
+        </form>
+    );
+}
--- client/src/services/UserService.ts
+++ client/src/services/UserService.ts
@@ -1,19 +1,32 @@
 
-import Api from "./api";
+import api from "./api";
 
-interface RegisterProps{
-    name:string,
-    email:string,
-    password:string
+interface RegisterProps {
+    name: string,
+    email: string,
+    password: string
 }
 
-class UserService{
-    register({name,email,password}:RegisterProps){
-      Api.post('/users/register',{
-        name:name,
-        email:email,
-        password:password
-      })
+interface LoginProps {
+    email: string,
+    password: string
+}
+
+
+class UserService {
+    async register({ name, email, password }: RegisterProps) {
+        await api.post('/users/register', {
+            name: name,
+            email: email,
+            password: password
+        })
+    }
+    async login({ email, password }: LoginProps) {
+        const response = await api.post('/users/login', {
+            email: email,
+            password: password
+        })   
+        localStorage.setItem("access_token",response.data.data.token)
     }
 }
 
